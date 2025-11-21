50edbafd8f37679e45c35462cc321833506b9205
Jhonatan Galeano <jhonatan.galeano@ghtcorp.com>
2025-11-20 20:29:28 -0500

    se comenta provider

--- providers.tf
+++ providers.tf
@@ -24,7 +24,7 @@ provider "aws" {
   # shared_credentials_files = [var.aws_shared_credentials_files]
   # profile = var.aws_profile
 
-  shared_credentials_files = var.aws_shared_credentials_files != "" ? [var.aws_shared_credentials_files] : null
-  profile = var.aws_profile != "" ? var.aws_profile : null
+  # shared_credentials_files = var.aws_shared_credentials_files != "" ? [var.aws_shared_credentials_files] : null
+  # profile = var.aws_profile != "" ? var.aws_profile : null
   
 }
